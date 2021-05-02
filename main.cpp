315bc9141914124d22b2bc969b333bf631b3c10f
Jernej Debevc <68189091+jernej-9@users.noreply.github.com>
2021-05-02 07:45:02 +0200

    Implement Ledger class with basic functionality

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 class Transaction
 {
@@ -36,11 +38,70 @@ private:
 	const std::string m_comment;
 };
 
+class Ledger
+{
+public:
+	Ledger(): m_names {}, m_transactions {}
+	{
+	}
+
+	void addTransaction(
+		const std::string payerName, const std::string recipientName,
+		const double amount, const std::string comment
+	)
+	{
+		m_transactions.push_back(
+			Transaction{
+				getID(payerName), getID(recipientName), amount, comment
+			}
+		);
+	}
+
+	void printTransactions()
+	{
+		for (Transaction trans : m_transactions)
+			trans.print();
+	}
+
+	// calculateBalances()
+	// settle()
+
+private:
+	int getID(std::string name)
+	{
+		// Check if name is already in m_names
+		auto found { std::find(m_names.begin(), m_names.end(), name) };
+
+		// If it is, the person already has an ID
+		if (found != m_names.end())
+		{
+			std::cout << "Name already exists\n";
+
+			// Return index of the name in m_names
+			return found - m_names.begin();
+		}
+		// If it is not, add the name to m_names and return new ID
+		else
+		{
+			std::cout << "New name\n";
+
+			m_names.push_back(name);
+			// New ID is the index of last element
+			return m_names.size() - 1;
+		}
+	}
+
+	// Array of names of the people involved. ID = i corresponds to m_names[i]
+	std::vector<std::string> m_names;
+
+	std::vector<Transaction> m_transactions;
+};
+
 int main()
 {
-	Transaction tran { 0, 1, 14.5, "For pizza" };
-
-	tran.print();
+	Ledger ledger;
+	ledger.addTransaction("Robert", "Mike", 33.28, "For pizza");
+	ledger.printTransactions();
 
 	return 0;
 }
